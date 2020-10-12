71c062049ba043244fcd1310c6c9b0cd8089aee9
charleswen68-code <charles_wen68@126.com>
2020-10-12 23:57:44 +0800

    change again

--- test.py
+++ test.py
@@ -24,4 +24,4 @@ descriptions:
 
 if __name__ == '__main__':
     print("Hello first python commit by vscode to GitHub")
-    print("I add another line here !")
+    print("I add another line here - and change it !")
